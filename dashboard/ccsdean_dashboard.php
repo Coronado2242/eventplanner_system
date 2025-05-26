b6543867a0ef613295feb306d99e0570b6501854
symonchristopher-bolinas <angelessymon@gmail.com>
2025-05-25 23:52:05 -0700

    modified:   dashboard/ccsdean_dashboard.php 	new file:   request/requirements.php

--- dashboard/ccsdean_dashboard.php
+++ dashboard/ccsdean_dashboard.php
@@ -270,7 +270,7 @@ session_start();
 <div id="requirementContent" style="display:none;">
     <main class="content" >
         <h1 style="margin-bottom: 0;">Requirements</h1>
-
+<iframe id="requirementsFrame" style="width:100%; height:600px; border:none;"></iframe>
 
     </main>
 </div>
@@ -331,6 +331,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("approvalFrame").src = "../request/forapproval.php";
+    });
+
+    document.querySelector(".toggle-btn").addEventListener("click", function () {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("collapsed");
+});
+document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("requirementsFrame").src = "../request/requirements.php";
     });
 
     document.querySelector(".toggle-btn").addEventListener("click", function () {
